0ba34975ed03fc7c232085bb0b9d0a7fadc65aa4
Ryan-Millard <millardryandevon@gmail.com>
2025-04-16 15:54:03 +0200

    update(Activities): Add Activity classes for each XML layout

--- app/src/main/java/com/example/budgiebudgettracking/MainActivity.kt
+++ app/src/main/java/com/example/budgiebudgettracking/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.budgiebudgettracking
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		findViewById<Button>(R.id.btnLogin).setOnClickListener {
+			startActivity(Intent(this, LoginActivity::class.java))
+		}
+
+		findViewById<Button>(R.id.btnRegister).setOnClickListener {
+			startActivity(Intent(this, RegisterActivity::class.java))
+		}
+
+		findViewById<Button>(R.id.btnCategory).setOnClickListener {
+			startActivity(Intent(this, CategoryActivity::class.java))
+		}
+
+		findViewById<Button>(R.id.btnExpense).setOnClickListener {
+			startActivity(Intent(this, ExpenseActivity::class.java))
+		}
+
+		findViewById<Button>(R.id.btnMonthlyBudget).setOnClickListener {
+			startActivity(Intent(this, MonthlyBudgetActivity::class.java))
+		}
+	}
 }
