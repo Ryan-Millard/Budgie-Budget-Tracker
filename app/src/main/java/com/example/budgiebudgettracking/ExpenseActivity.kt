a447149fc366cb13c4449b57aeb4e6ce75af7fce
Ryan-Millard <millardryandevon@gmail.com>
2025-04-17 16:25:48 +0200

    update(ExpenseActivity): Add FloatingActionButton + to ExpenseCalculatorActivity

--- app/src/main/java/com/example/budgiebudgettracking/ExpenseActivity.kt
+++ app/src/main/java/com/example/budgiebudgettracking/ExpenseActivity.kt
@@ -1,6 +1,8 @@
 package com.example.budgiebudgettracking
 
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.content.Intent
 
 import com.example.budgiebudgettracking.BaseActivity
 
@@ -8,5 +10,14 @@ class ExpenseActivity : BaseActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_expense)
+
+		setupClickListeners()
+	}
+
+	private fun setupClickListeners() {
+		val addExpenseFab = findViewById<FloatingActionButton>(R.id.addExpenseFab)
+		addExpenseFab.setOnClickListener {
+			startActivity(Intent(this, ExpenseCalculatorActivity::class.java))
+		}
 	}
 }
